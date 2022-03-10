fd6c7c8b6cd8a14f39a0d29ca3691ad596ec66f8
Santiago Luis Ocón <ocon.santiago@gmail.com>
2022-03-10 17:56:15 -0300

    First project layout.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './pages/Login/Login';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Test</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/TestData/user.js
@@ -0,0 +1,7 @@
+export const users = [
+  { username: 'test', password: 'test' },
+  { username: 'santiago', password: 'ocon' },
+  { username: 'gabriel', password: 'gonzalez' },
+  { username: 'carlos', password: 'spagnolo' },
+  { username: 'lourdes', password: 'dupuy' },
+];
