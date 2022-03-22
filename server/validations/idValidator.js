fbeaa96421e4fbcf3150cc07530aa501b648ce5f
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-03-22 11:05:22 -0300

    Created id validations

--- server/validations/idValidator.js
+++ server/validations/idValidator.js
@@ -4,6 +4,18 @@ const idSchema = Joi.alternatives().try(
   Joi.object({
     userId: Joi.string().alphanum().min(24).max(24).required(),
   }),
+  Joi.object({
+    adoptionPetId: Joi.string().alphanum().min(24).max(24).required(),
+  }),
+  Joi.object({
+    foundPetId: Joi.string().alphanum().min(24).max(24).required(),
+  }),
+  Joi.object({
+    lostPetId: Joi.string().alphanum().min(24).max(24).required(),
+  }),
+  Joi.object({
+    matchPetId: Joi.string().alphanum().min(24).max(24).required(),
+  }),
 );
 
 module.exports = idSchema;
