486179ef090e599c98883eff4442a3dfc8ccbb28
Santiago Luis Ocón <ocon.santiago@gmail.com>
2022-03-24 11:17:03 -0300

    Cloudinary const added to FoundPetController.

--- server/controllers/foundPetController.js
+++ server/controllers/foundPetController.js
@@ -1,3 +1,10 @@
+const cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const foundPetController = (FoundPet) => {
   //GET
   const getFoundPets = async (req, res) => {
